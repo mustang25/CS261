6e48779158bf9da8c1938700d144614c5855262d
Rob Navarro <robnavarro25@gmail.com>
2016-04-10 14:49:07 -0600

    file changes

--- A1/Q0.c
+++ A1/Q0.c
@@ -3,32 +3,32 @@
  * Date: January 14, 2016
  * Solution description: Printer different values using pointers.
  */
- 
+
 #include <stdio.h>
 #include <stdlib.h>
 
 void fooA(int* iptr){
-     /*Print the value pointed to by iptr*/
-    printf("The value pointed to by iptr is: %d\n", *iptr);
-     /*Print the address pointed to by iptr*/
-    printf("The address pointed to by iptr is: %p\n", iptr);
-     /*Print the address of iptr itself*/
-    printf("The address of iptr is: %p\n", &iptr);
+        /*Print the value pointed to by iptr*/
+        printf("The value pointed to by iptr is: %d\n", *iptr);
+        /*Print the address pointed to by iptr*/
+        printf("The address pointed to by iptr is: %p\n", iptr);
+        /*Print the address of iptr itself*/
+        printf("The address of iptr is: %p\n", &iptr);
 }
 
 int main(){
-    
-    /*declare an integer x*/
-    int x = 5;
-    
-    /*print the address of x*/
-    printf("The address of x is: %p\n", &x);
-    
-    /*Call fooA() with the address of x*/
-    fooA(&x);
-    
-    /*print the value of x*/
-    printf("The value of x is: %d\n", x);
-    
-    return 0;
+
+        /*declare an integer x*/
+        int x = 5;
+
+        /*print the address of x*/
+        printf("The address of x is: %p\n", &x);
+
+        /*Call fooA() with the address of x*/
+        fooA(&x);
+
+        /*print the value of x*/
+        printf("The value of x is: %d\n", x);
+
+        return 0;
 }
